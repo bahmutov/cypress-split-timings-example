f2ffb18d6a74ff93146bea4b4fce9189b935e8a7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-10-17 08:56:39 -0400

    turn on debugging

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     // baseUrl, etc
     supportFile: false,
     fixturesFolder: false,
+    video: false,
     setupNodeEvents(on, config) {
       cypressSplit(on, config)
       // IMPORTANT: return the config object
