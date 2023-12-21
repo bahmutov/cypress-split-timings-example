c09ab23c67b83889767eda9ed8c15088fead5556
Tony <tony.wu@myzeller.com>
2023-12-21 21:22:33 +1100

    change config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,43 @@
 import { defineConfig } from 'cypress'
 import cypressSplit from 'cypress-split'
 
+const setupNodeEvents = (cypressOn, config) => {
+  cypressSplit(cypressOn, config)
+  // IMPORTANT: return the config object
+  return config
+}
 module.exports = defineConfig({
   e2e: {
-    // baseUrl, etc
-    supportFile: false,
-    fixturesFolder: false,
-    video: false,
-    setupNodeEvents(on, config) {
-      cypressSplit(on, config)
-      // IMPORTANT: return the config object
-      return config
-    },
+    // // baseUrl, etc
+    // baseUrl: 'http://localhost:3000',
+    // supportFile: false,
+    // fixturesFolder: false,
+    // video: false,
+    // setupNodeEvents(on, config) {
+    //   cypressSplit(on, config)
+    //   // IMPORTANT: return the config object
+    //   return config
+    // },
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 20000,
+    // reporter: 'mochawesome',
+    // reporterOptions: {
+    //   html: true,
+    //   json: true,
+    //   overwrite: false,
+    //   quiet: true,
+    //   reportDir: 'cypress/reports',
+    //   reportFilename: 'mocha-report',
+    //   reportTitle: 'Merchant Portal - Cypress tests result',
+    //   toConsole: true,
+    // },
     specPattern: 'cypress/**/*.cy.js',
+    setupNodeEvents,
+    // supportFile: 'cypress/support/e2e.ts',
+    supportFile: false,
+    video: false,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    blockHosts: ['www.google-analytics.com', 'www.googletagmanager.com'],
   },
 })
